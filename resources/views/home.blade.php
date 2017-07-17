b6986033953044475bd46e29fd24e0c10019decd
Nina Kristel Cheng <nkmcheng@live.com>
2017-07-17 18:34:01 +0800

    updated content and responsive

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -38,6 +38,7 @@
                   map: map,
                   icon: image
                 });
+                map.setOptions({ scrollwheel: false });
 
                 marker.addListener('click', toggleBounce);
             }
@@ -53,6 +54,7 @@
 
     </head>
     <body>
+
         <nav class="navbar navbar-default navbar-fixed-top" id="nav_bar">
             <div class="container-fluid">
                 <div class="navbar-header">
@@ -69,7 +71,7 @@
                 <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                     <ul class="nav navbar-nav navbar-right">
                         <li><a href="#home">HOME</a></li>
-                        <li><a href="#about">ABOUT</a></li>
+                        <li><a href="#aboutus">ABOUT</a></li>
                         <li><a href="#products">PRODUCTS</a></li>
                         <li><a href="#partners">PARTNERS</a></li>
                         <li><a href="#contact-us" class="btn btn-primary btn-flat">CONTACT US</a></li>
@@ -86,10 +88,9 @@
             <div class="target" id="home">
                 <div class="line"></div>
                 <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 text-content">
-                    <p class="sub-header">OUR NEXT PROJECT:</p>
-                    <p class="text-header">YOUR NEW WEBSITE</p>
+                    <p class="text-header">Cloudwalk Digital</p>
 
-                    <p>Let's create something that will convert visitors to instant customers. With clean and clear UI/UX and branded aesthetics, create valuable qualified leads.</p>
+                    <p>We are a digital solutions company that creates products to improve and innovate companies across the globe. We create unparalleled systematic business solutions to make your operations more efficient.</p>
                     <button type="button" class="btn btn-blue btn-flat btn-lg">GET ESTIMATE</button>
                 </div>
                 <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
@@ -98,14 +99,15 @@
             </div>
 
             <!-- ABOUT -->
-            <div id="about" class="container-fluid target">
-                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 text-content">
+            <div id="aboutus" class="container-fluid target">
+                <div class="col-lg-5 col-md-offset-1 col-md-5 col-sm-5 col-sm-offset-1 col-xs-12 text-content">
                     <p>Expand your brand's reach today, take it to the clouds! We'll be happy to do the heavy lifting (coding) for you!</p>
                     <hr>
                 </div>
-                <div class="col-lg-5 col-md-5 col-sm-6 col-xs-12 logo">
+                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 logo">
                     <img src="/assets/images/logo.png" alt="LOGO" class="img-responsive">
-                    <p>We are digital solutions company that create products to improve and innovate companies across the globe. We create unparalleled systematic business solutions to make your operations more efficient.</p>
+                    <p>CLOUDWALK DIGITAL has developed technologically advanced software solutions over the past years with a team of competent software engineers who are providing their best efforts to satisfy the needs of its customers. One of its own top of the line software solutions for the healthcare industry, MEDIX, is already used by leading healthcare providers in the country and worldwide. </p>
+                    <p>We aim to create a smart country working efficiently as a team to improve the quality of life for many people while we embrace the digital technology and we make it a point that it will give unmatched benefits to our clients, making their lives and businesses easier and more manageable.</p>
                 </div>
                 <div class="col-lg-12 col-sm-12 col-xs-12 feature">
                     <div class="container">
@@ -137,25 +139,29 @@
                     <div class="item">
                         <img src="/assets/images/photolive-logo.png" alt="Photolive">
                         <div class="slick-caption">
-                            <p>Products</p>
-                            <span>Photolive</span> is a social media tool and the first digital photo booth in the country that allows brands to gain valuable presence in top social networking sites like Facebook, Instagram, Twitter and email in real time. <br>
+                            <span>Photolive</span> a first of its kind modern, digital photo booth that combines sophisticated design with the latest in digital technology creating a memorable experience for you and your guests. <br>
                             <a href="http://photolive.com.ph/" target=_blank class="btn btn-blue btn-flat btn-lg">VISIT SITE</a>
                         </div>
                     </div>
                     <div class="item">
-                        <img src="/assets/images/bolooka-logo.png" alt="Bolooka" width="250" class="landscape-img">
+                        <img src="/assets/images/bolooka-logo.png" alt="Bolooka" width="250">
                         <div class="slick-caption">
-                            <p>Products</p>
                             <span>Bolooka</span> is an e-commerce platform that provides an exclusive marketplace for local merchants and artists who create only the best products in the country. <br>
                             <a href="http://www.bolooka.com/" target=_blank class="btn btn-blue btn-flat btn-lg">VISIT SITE</a>
                         </div>
                     </div>
                     <div class="item">
-                        <img src="/assets/images/medix-logo.png" alt="Medix" width="250" class="landscape-img">
+                        <img src="/assets/images/medix-logo.png" alt="Medix" width="250">
                         <div class="slick-caption">
-                            <p>Products</p>
-                            <span>Medix</span> is a cloud-based clinic management software that helps healthcare practitioners improve their clinic operations using just one quick and easy service. It is a 24/7 accessible manager that efficiently organizes appointments and clinic records. <br>
+                            <span>Medix</span> is a cloud-based clinic management software that helps healthcare practitioners improve their clinic operations using just one quick and easy service. <br>
                             <a href="https://www.medix.ph/" target=_blank class="btn btn-blue btn-flat btn-lg">VISIT SITE</a>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <img src="/assets/images/verify-logo.png" alt="Verify" width="250">
+                        <div class="slick-caption">
+                            <span>Verify</span> is a platform that helps companies and organizations track and record the activities and performance deployed manpower and teams <br>
+                            <a href="http://project-verify.medix.ph/" target=_blank class="btn btn-blue btn-flat btn-lg">VISIT SITE</a>
                         </div>
                     </div>
                 </div>
@@ -173,10 +179,10 @@
                     <p class="text-header"><span class="quote">“</span>The best Digital Agency since sliced bread!</p>
                     <p class="text-description">How’s business? Boomin. I’m giving you cloth talk, cloth. Special cloth alert, cut from a special cloth. Surround yourself with angels.</p>
                     <div class="personal-information">
-                        <div class="col-md-4 col-sm-6 col-xs-4">
+                        <div class="col-md-4 col-sm-6 col-xs-12">
                             <img src="/assets/images/pom.jpg" alt="Medix" class="img-circle" width="150" height="150">
                         </div>
-                        <div class="col-md-8 col-sm-6 col-xs-8 info">
+                        <div class="col-md-8 col-sm-6 col-xs-12 info">
                             <span class="name">Jonathan L. Simmons</span> <br>
                             <span class="company">Blade and Babe Inc.</span>
                         </div>
@@ -186,12 +192,12 @@
 
             <!-- CONTACT US -->
             <div id="contact-us" class="container-fluid target">
-                <div class="col-lg-5 col-md-5 col-sm-5 col-xs-12 text-content">
+                <div class="col-lg-5 col-md-5 col-sm-4 col-xs-12 text-content">
                     <p class="text-header">Get a Free Consultation</p>
                     <hr><br>
                     <p class="text-description">Get in touch with us today! Fill out this form and let's get things done.</p>
                 </div>
-                <div class="col-lg-7 col-md-7 col-sm-7 col-xs-12 form-container">
+                <div class="col-lg-7 col-md-7 col-sm-8 col-xs-12 form-container">
                     <div class="well">
                         <form class="row">
                             <div class="col-md-6 col-sm-6 col-xs-12">
@@ -231,7 +237,7 @@
                 <div class="well well-lg">
                     <p class="text-header">DROP BY OUR OFFICE!</p>
                     <hr><br>
-                    <p class="text-description">79 3BB Building Kamuning Street Corner,<br>11th Jamboree, Quezon City, 1100 Metro Manila</p>
+                    <p class="text-description">Unit 1 3BB Building, #79 Kamuning Road, <br> Brgy. Kamuning, Quezon City, 1103</p>
                 </div>
                 <div id="map"></div>
             </div>
@@ -242,7 +248,7 @@
                 <div class="col-md-8 col-sm-6 col-xs-12 text-content">
                     <p class="text-header">DROP BY OUR OFFICE!</p>
                     <p>whatsup@cloudwalkdigital.com</p>
-                    <p>(02) 414 4014</p>
+                    <p>(02)-962-4192</p>
                     <button type="button" class="btn btn-blue btn-flat">START YOUR PROJECT</button>
                 </div>
                 <div class="col-md-4 col-sm-6 col-xs-12 socials">
@@ -263,6 +269,7 @@
 
         <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAueyHwdNHm7u2X68gb-4YuncJzRFDoyYI&callback=initMap"></script>
         <script src="{{ asset('js/jquery.min.js') }}"></script>
+        <script src="{{ asset('js/bootstrap.min.js') }}"></script>
         <script src="{{ asset('css/slick/slick.min.js') }}" ></script>
         <script>
             $(document).ready(function(){
