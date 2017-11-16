c15a145a25859baa0d81311bb1e8875a0a3fd946
Niña Kristel M. Cheng <nkmcheng@live.com>
2017-11-16 14:21:08 +0800

    updated validator

--- app/Http/Controllers/ContactUsController.php
+++ app/Http/Controllers/ContactUsController.php
@@ -2,10 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\SendContactUsRequest;
 use App\Notifications\RegistrationSuccess;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
-use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Validator;
 
 class ContactUsController extends Controller
@@ -15,19 +15,17 @@ class ContactUsController extends Controller
      *
      * @param  Request  $request
      */
-    public function sendEmail(Request $request)
+    public function sendEmail(SendContactUsRequest $request)
     {
-        Validator::make($request->all(), [
-            'g-recaptcha-response' => 'required|recaptcha',
-        ])->validate();
-
         $input = $request->all();
 
-        $emails = ['bm@cloudwalkdigital.com', 'mm@cloudwalkdigital.com'];
+        $emails = ['mm@cloudwalkdigital.com'];
+        $ccMails = ['bm@cloudwalkdigital.com'];
         $bccMail = ['leo.a@cloudwalkdigital.com', 'nina.c@cloudwalkdigital.com'];
 
         foreach( $emails as $email) {
             Mail::to($email)
+                ->cc($ccMails)
                 ->bcc($bccMail)
                 ->send(new RegistrationSuccess($input));
         }
